0327a49cd2f13d077e18e458c1bc3b30d3942ad9
Greg Rynkowski <greg@twobuffers.com>
2021-04-23 21:54:26 +0100

    Add process fn (1st ver)

--- app/main/src/video_stt/script.clj
+++ app/main/src/video_stt/script.clj
@@ -2,8 +2,11 @@
   (:require
     [clojure.spec.alpha :as spec]
     [integrant.core :as ig]
+    [me.raynes.fs :as fs]
+    [utils.fs :refer [write-lines]]
     [video-stt.gcp-java.speech :as gcp-speech]
-    [video-stt.gcp-java.storage :as gcp-storage]))
+    [video-stt.gcp-java.storage :as gcp-storage]
+    [video-stt.audio-cli :as audio-cli]))
 
 ;; ---- SPEC --------------------
 
@@ -11,11 +14,40 @@
 
 ;; ---- BODY --------------------=
 
-(defn process
-  [speech storage
-   {:keys [video output] :as params}]
-  (println "Processing..." params)
-  {})
+;; 1. take audio from video
+;; 2. copy audio to Google Storage
+;; 3. call Speech API
+;; 4. write down the txt
+
+(defn process [speech storage {:keys [video output]}]
+  (println "Processing...")
+  (let [
+        tmp-dir  (fs/temp-dir "video-stt")
+        _ (prn "+ tmp dir created:" tmp-dir)
+        audio-res (audio-cli/extract-audio {:input video :output-dir (str tmp-dir)})
+        _ (prn "+ audio-res:" audio-res)
+        audio-file (:output audio-res)
+        upload-res (gcp-storage/upload-file storage audio-file "rynkowski-tmp-audio")
+        _ (prn "+ upload-res:" upload-res)
+        speech-res (gcp-speech/long-to-text speech (gcp-storage/->uri upload-res))
+        _ (prn "+ speech-res:" speech-res)
+        _ (write-lines output speech-res)
+        ]
+
+    (gcp-storage/delete-blob (gcp-storage/->blob storage upload-res))
+    _ (prn "+ GCP file removed:" (gcp-storage/->uri upload-res))
+    (fs/delete audio-file)
+    _ (prn "+ local file removed:" audio-file)
+    (fs/delete-dir tmp-dir)
+    _ (prn "+ tmp dir removed:" tmp-dir)))
+
+(comment
+  (do (require '[dev])
+      (def f (-> (dev/state) (::fn)))
+      f)
+  (f {:video "/Users/greg/Desktop/sample.mp4"
+      :output "/Users/greg/Desktop/sample.txt"})
+  )
 
 ;; ---- DI ----------------------
 
