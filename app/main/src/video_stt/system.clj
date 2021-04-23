7674fa8a4db3f5311a065b647b759b773e636fe9
Greg Rynkowski <greg@twobuffers.com>
2021-04-23 16:44:15 +0100

    Add injecting cred file

--- app/main/src/video_stt/system.clj
+++ app/main/src/video_stt/system.clj
@@ -5,8 +5,8 @@
     [video-stt.gcp-java.storage :as gcp-storage]
     [integrant.core :as ig]))
 
-(defn system-config []
-  {::gcp-cred/instance    {:path "stt-playground-310715-193109fadcd0-stt-processor-4.json"}
+(defn system-config [cred]
+  {::gcp-cred/instance    {:path cred}
    ::gcp-speech/instance {:cred (ig/ref ::gcp-cred/instance)}
    ::gcp-storage/instance {:cred (ig/ref ::gcp-cred/instance)}}
   )
